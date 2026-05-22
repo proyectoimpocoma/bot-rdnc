1a4019f1cd1045455a5ee0361b8dfba1e22668ca
luissoler <luissoler@gmail.com>
2026-05-22 15:32:55 -0500

    Cambio  de headless browser a True

--- app/scrapper/browser.py
+++ app/scrapper/browser.py
@@ -7,7 +7,7 @@ async def new_rndc_page():
     """Crea una nueva página de Playwright para interactuar con el sitio web de RNDC."""
     playwright = await async_playwright().start()
     browser = await playwright.chromium.launch(
-        headless=False, channel="chrome", timeout=30000
+        headless=True, channel="chrome", timeout=30000
     )
     context = await browser.new_context(accept_downloads=True)
     page = await context.new_page()
